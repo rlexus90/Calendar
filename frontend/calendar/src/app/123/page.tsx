836d1f18692966d41debb260de5b15b88973700f
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-11-22 22:19:09 +0100

    feat: add calendar

---
+++ frontend/calendar/src/app/123/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { parseDate } from '@internationalized/date';
+import { Calendar } from '@nextui-org/calendar';
+import type { DateValue } from '@react-types/calendar';
+import  { useEffect, useState } from 'react';
+
+
+export default function Page123() {
+	const [value, setValue] = useState<DateValue>(parseDate("2024-11-22"));
+
+	useEffect(()=>{
+		console.log(value)
+	},[value])
+  return (
+    <>
+      <div>
+        <div className="items-center justify-items-center mx-auto w-32 p-5">
+        <p>Page 123</p>  
+				 <Calendar 
+      aria-label="Date (Controlled)" 
+      value={value} 
+      onChange={setValue} 
+    />
+        </div>  
+       
+      </div>
+    </>
+  );
+}
