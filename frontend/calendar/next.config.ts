fa30d2d83778a592383a7587c46092d17f182fbc
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-11-23 10:51:31 +0100

    feat: add calendar

--- frontend/calendar/next.config.ts
+++ frontend/calendar/next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
+// import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+const PWA= withPWA({
+	dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports=  PWA({
+	reactStrictMode: true,
+	compiler: {
+		removeConsole: process.env.NODE_ENV !== 'development',
+	},
+});
