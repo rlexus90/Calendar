20181668688eba659508f35a978b4612e57b51ca
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-11-24 15:31:36 +0100

    feat: test PWA

--- frontend/calendar/src/app/123/page.tsx
+++ frontend/calendar/src/app/123/page.tsx
@@ -2,28 +2,28 @@
 
 import Link from "next/link";
 
-// import { parseDate } from '@internationalized/date';
-// import { Calendar } from '@nextui-org/calendar';
-// import type { DateValue } from '@react-types/calendar';
-// import  { useEffect, useState } from 'react';
+import { parseDate } from '@internationalized/date';
+import { Calendar } from '@nextui-org/calendar';
+import type { DateValue } from '@react-types/calendar';
+import  { useEffect, useState } from 'react';
 
 
 export default function Page123() {
-	// const [value, setValue] = useState<DateValue>(parseDate("2024-11-22"));
+	const [value, setValue] = useState<DateValue>(parseDate("2024-11-22"));
 
-	// useEffect(()=>{
-	// 	console.log(value)
-	// },[value])
+	useEffect(()=>{
+		console.log(value)
+	},[value])
   return (
     <>
       <div>
         <div className="items-center justify-items-center mx-auto p-5 max-w-28">
         <p>Page 123</p>  
-				 {/* <Calendar 
+				 <Calendar 
       aria-label="Date (Controlled)" 
       value={value} 
       onChange={setValue} 
-    /> */}
+    />
 				<Link
             className="px-3 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="/"
